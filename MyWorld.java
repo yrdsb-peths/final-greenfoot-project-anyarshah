8d104ab2214c225082279a504e075dda728590cb
anyarshah <348612037@gapps.yrdsb.ca>
2022-12-19 12:24:45 -0500

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -3,12 +3,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class MyWorld here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Anya Shah 
+ * @version 12/19/2022
  */
 public class MyWorld extends World
 {
-
     /**
      * Constructor for objects of class MyWorld.
      * 
