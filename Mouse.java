997a50cc5e99d3fc0a201b0c4e3a3ee912eb6598
anyarshah <348612037@gapps.yrdsb.ca>
2022-12-19 20:04:06 -0500

    Spawn mouse
    Randomly adds mouse onto the world.

--- Mouse.java
+++ Mouse.java
@@ -8,12 +8,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Mouse extends Actor
 {
-    /**
-     * Act - do whatever the Mouse wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act()
     {
-        
+        int x = getX();
+        int y = getY();
+        setLocation(x, y);
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -14,9 +14,18 @@ public class MyWorld extends World
      */
     public MyWorld()
     {   
-        super(600, 500, 1, false);
+        super(600, 500, 1, false); 
         
         Snake snake = new Snake();
         addObject(snake, 100, 400);
+        // Spawns a mouse randomly in the world.
+        spawnMouse();
+    }
+    public void spawnMouse()
+    {
+        Mouse mouse = new Mouse();
+        int x = Greenfoot.getRandomNumber(501);
+        int y = Greenfoot.getRandomNumber(401);
+        addObject(mouse, x + 50, y + 50);
     }
 }
