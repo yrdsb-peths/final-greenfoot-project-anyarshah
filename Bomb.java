5c06e1fc9360b7d6f7f3a5c59732b141ecb38cd5
anyarshah <348612037@gapps.yrdsb.ca>
2022-12-22 13:01:55 -0500

    Update Bomb.java

--- Bomb.java
+++ Bomb.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Bomb extends Actor
 {
-    int speed = 4;
+    int speed = 1;
     public void act()
     {
         int x = getX();
