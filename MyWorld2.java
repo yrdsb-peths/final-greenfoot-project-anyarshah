776f5e61c627b78c8c267b28e419bcdc40d3c738
anyarshah <348612037@gapps.yrdsb.ca>
2022-12-27 16:33:43 -0500

    Update MyWorld2.java

--- MyWorld2.java
+++ MyWorld2.java
@@ -4,7 +4,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * Level 2 of the game: The world where the astronaut, star, and asteroid exist.
  * 
  * @author Anya Shah
- * @version 12/26/2022
+ * @version 12/27/2022
  */
 public class MyWorld2 extends World
 {
