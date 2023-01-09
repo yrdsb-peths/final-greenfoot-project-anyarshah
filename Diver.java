1aff3f5aa9b049d36cc217782d2d76ad5fea18fc
anyarshah <348612037@gapps.yrdsb.ca>
2023-01-09 13:10:01 -0500

    Add diver, fish, and shark

--- Diver.java
+++ Diver.java
@@ -1,12 +1,12 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * The shark, obstacle to the diver.
+ * The diver.
  * 
  * @author Anya Shah
  * @version 01/09/2023
  */
-public class Shark extends Actor
+public class Diver extends Actor
 {
     public void act()
     {
--- Fish.java
+++ Fish.java
@@ -1,17 +1,13 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Fish here.
+ * The fish.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Anya Shah 
+ * @version 01/09/2023
  */
 public class Fish extends Actor
 {
-    /**
-     * Act - do whatever the Fish wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act()
     {
         // Add your action code here.
