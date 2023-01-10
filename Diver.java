324864df866d35df658bd5afdba40bfaeb094851
anyarshah <348612037@gapps.yrdsb.ca>
2023-01-10 13:00:52 -0500

    Update Diver.java
    what happens when the diver touches the fish and shark

--- Diver.java
+++ Diver.java
@@ -23,9 +23,37 @@ public class Diver extends Actor
             setRotation(90);
             move(2);
         }
+        save();
     }
     public void save()
     {
-        
+        MyWorld3 world = (MyWorld3) getWorld();
+        /**
+         * Removes the fish from the screen when the diver saves it.
+         * Spawns another fish.
+         * Increases the score.
+         * Spawns another shark.
+         */
+        if(isTouching(Fish.class))
+        {
+            removeTouching(Fish.class);
+            world.spawnFish();
+            world.increaseScore();
+            // Every time the diver saves ten fish, another shark spawns. 
+            if(world.score % 10 == 0)
+            {
+                world.spawnShark();
+            }
+        }
+        /**
+         * If diver touches shark:
+         * Game over appears on screen.
+         * Game stops.
+         */
+        if(isTouching(Shark.class))
+        {
+            world.gameOver();
+            Greenfoot.stop();
+        }
     }
 }
