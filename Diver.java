8086e69880dec17221bda95566633adbc8d46114
anyarshah <348612037@gapps.yrdsb.ca>
2023-01-10 12:39:20 -0500

    Move diver

--- Diver.java
+++ Diver.java
@@ -4,12 +4,24 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * The diver.
  * 
  * @author Anya Shah
- * @version 01/09/2023
+ * @version 01/10/2023
  */
 public class Diver extends Actor
 {
+    /**
+     * Moves the astronaut up and down using the arrow keys.
+     */
     public void act()
     {
-        // Add your action code here.
+        if(Greenfoot.isKeyDown("up"))
+        {
+            setRotation(270);
+            move(2);
+        }
+        if(Greenfoot.isKeyDown("down"))
+        {
+            setRotation(90);
+            move(2);
+        }
     }
 }
